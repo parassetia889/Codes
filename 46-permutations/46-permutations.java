a0d1ac0ca6bc3297e4c058c5748c00de7b0e35bb
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-01-25 13:27:22 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (91.16%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,34 +1,37 @@
-import java.util.*;
 class Solution {
-    public void solve(int[] nums, int index, List<List<Integer>> ans )
-    {
-     if(index >= nums.length){
-        ans.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
-         return ;
-       }
-         
- 
-        for(int i = index; i<nums.length; i++)
-        {
-            swap(nums,index,i);
-            solve(nums, index+1, ans);
-            //backtrack
-             swap(nums,index,i);
-        }
-            
-    }
-   public void swap(int[] a, int i, int j) {
-    int temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
-}
     
+    public void solve(int[] nums, List<List<Integer>> ans, int index){
+        if(index >= nums.length){
+            ans.add(toList(nums));
+            return;
+        }
+        
+        for(int i = index; i<nums.length; i++){
+            swap(nums,index,i);
+            solve(nums, ans, index+1);
+                
+                //backtrack
+             swap(nums,index,i);    
+        }
+    }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        int index = 0 ;
-        solve(nums, index, ans);
-        
+        int index = 0;
+        solve(nums, ans, index);
         return ans;
+    }
+    
+    public List<Integer> toList(int[] nums){
+        List<Integer> res = new ArrayList<>();
+        for(int i:nums)
+            res.add(i);
         
+        return res;
+    }
+    
+    public void swap(int[] nums,int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
