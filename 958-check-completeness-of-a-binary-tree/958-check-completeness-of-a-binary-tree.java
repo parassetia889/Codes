f555282ffe18045ca49a8e04093e89742d5e4f71
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-04 01:23:31 +0530

    Time: 1 ms (79.84%), Space: 42.9 MB (5.63%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.java
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.java
@@ -14,36 +14,28 @@
  * }
  */
 class Solution {
-     private static int countNodes(TreeNode root){
-        if(root == null)
-            return 0;
-
-        int leftNodes = countNodes(root.left);
-        int rightNodes = countNodes(root.right);
-
-        return (1 + leftNodes + rightNodes);
-    }
     public boolean isCompleteTree(TreeNode root) {
-
-        int count = countNodes(root);
-        return helper(root, 0, count);
-    }
-
-    private static boolean helper(TreeNode root, int index, int numberOfNodes) {
         
-        //other will take care
-        if(root == null)
-            return true;
-
-        //index moves out of range
-        if(index >= numberOfNodes)
-            return false;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
         
-        boolean isLeftCompleteBinaryTree = helper(root.left, 2*index +1, numberOfNodes);
-        boolean isRightCompleteBinaryTree = helper(root.right, 2*index +2, numberOfNodes);
-
-        //if right and left are complete binary tree
-        // then we can say that the whole tree is a complete binary tree
-        return isLeftCompleteBinaryTree && isRightCompleteBinaryTree;
+        boolean last = false;
+        
+       
+        while(!q.isEmpty()){
+            TreeNode temp = q.remove();
+            
+            if(temp == null)
+                last = true;
+            else{
+                if(last)
+                    return false;
+                
+                q.add(temp.left);
+                q.add(temp.right);
+            }
+        }
+        
+        return true;
     }
 }
