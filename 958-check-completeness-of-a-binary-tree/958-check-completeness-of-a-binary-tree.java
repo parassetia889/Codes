a543c4debff9905a008d7342e875f3e35d653ae4
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-04 01:08:18 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (8.55%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.java
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.java
@@ -30,15 +30,20 @@ class Solution {
     }
 
     private static boolean helper(TreeNode root, int index, int numberOfNodes) {
-
+        
+        //other will take care
         if(root == null)
             return true;
 
+        //index moves out of range
         if(index >= numberOfNodes)
             return false;
+        
         boolean isLeftCompleteBinaryTree = helper(root.left, 2*index +1, numberOfNodes);
         boolean isRightCompleteBinaryTree = helper(root.right, 2*index +2, numberOfNodes);
 
-        return isLeftCompleteBinaryTree & isRightCompleteBinaryTree;
+        //if right and left are complete binary tree
+        // then we can say that the whole tree is a complete binary tree
+        return isLeftCompleteBinaryTree && isRightCompleteBinaryTree;
     }
 }
