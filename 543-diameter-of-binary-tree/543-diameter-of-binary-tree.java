2ca11797100fb14b84ba35a2143bc291d4aaec70
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-04 14:55:21 +0530

    Time: 1 ms (49.81%), Space: 42.9 MB (9.15%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -1,39 +1,34 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     
-    int maxDiameter = 0;
+    int diameter = 0;
+    
     public int height(TreeNode root){
         
-        if(root == null)
-            return 0;
+        // Algorithm: Result = Max value of (1 + left_subtree_height + right_subtree_height)
         
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
+        // Step 1: Base case
+        if(root == null){ return 0; }
         
+        // Step 2: Calculate height of left sub tree
+        int leftSubTreeHeight = height(root.left);
         
-        maxDiameter = Math.max(maxDiameter, 1 + leftHeight + rightHeight);
+        // Step 3: Calculate height of right sub tree
+        int rightSubTreeHeight = height(root.right);
         
-        return (Math.max(leftHeight, rightHeight) + 1);
+        // Step 4: Find & store max value of (1 + left_height + right_height)
+        diameter = Math.max(diameter, leftSubTreeHeight + rightSubTreeHeight);
+        
+        // Step 5: Return height of current node
+        return 1 + Math.max(leftSubTreeHeight, rightSubTreeHeight);
+        
     }
     
     public int diameterOfBinaryTree(TreeNode root) {
         
+        // Step 0 : Calculate the height at each level & store the max value in 'diameter'
         height(root);
         
-        return maxDiameter-1;
+        return diameter ;
+        
     }
 }
