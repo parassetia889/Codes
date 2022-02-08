778ecc13a7f146561475733c7f0c3f43be76bf32
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-08 11:44:06 +0530

    Time: 48 ms (40.09%), Space: 68.8 MB (58.07%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -0,0 +1,42 @@
+class Solution {
+     public int maxNumberOfFamilies(int n, int[][] reservedSeats){
+
+        Map<Integer, List<Integer>> reservedSeatMap = new HashMap<>();
+        int ans = 0;
+
+        for(int [] row : reservedSeats){
+            if(reservedSeatMap.containsKey(row[0])){
+                reservedSeatMap.get(row[0]).add(row[1]);
+            }else{
+                reservedSeatMap.put(row[0], new ArrayList<>(Arrays.asList(row[1])));
+            }
+        }
+        //each complete empty row can
+        //accomodate 2 groups
+        ans = (n-reservedSeatMap.size())*2;
+
+        for(List<Integer> reservedSeatsList : reservedSeatMap.values()){
+
+            boolean isSettled = false;
+            if(!reservedSeatsList.contains(2) && !reservedSeatsList.contains(3)
+            && !reservedSeatsList.contains(4) && !reservedSeatsList.contains(5)) {
+                ans++;
+                isSettled = true;
+            }
+            if(!reservedSeatsList.contains(6) && !reservedSeatsList.contains(7)
+                    && !reservedSeatsList.contains(8) && !reservedSeatsList.contains(9)) {
+                ans++;
+                isSettled = true;
+            }
+            if(!isSettled){
+                if(!reservedSeatsList.contains(4) && !reservedSeatsList.contains(5)
+                        && !reservedSeatsList.contains(6) && !reservedSeatsList.contains(7)) {
+                    ans++;
+                   
+                }
+            }
+        }
+        return  ans;
+
+    }
+}
