5da152250265b1073f52361c47e7382d2a2de558
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-13 11:23:40 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (24.65%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,28 +1,36 @@
 class Solution {
-  List<List<Integer>> output = new ArrayList();
-  int n, k;
-
-  public void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
-    // if the combination is done
-    if (curr.size() == k) {
-      output.add(new ArrayList(curr));
-      return;
+    List<List<Integer>> ans;
+    List<Integer> output;
+    
+     void solve(int[] nums, List<Integer> output, int index  )
+    {
+        
+        //base case
+        if(index >= nums.length){
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+        
+        //exclude
+        solve(nums, output, index+1);
+        
+        //include
+        int element = nums[index];
+        output.add(element);
+        solve(nums, output, index+1);
+        output.remove(output.size()-1);
     }
-    for (int i = first; i < n; ++i) {
-      // add i into the current combination
-      curr.add(nums[i]);
-      // use next integers to complete the combination
-      backtrack(i + 1, curr, nums);
-      // backtrack
-      curr.remove(curr.size() - 1);
+    
+    public List<List<Integer>> subsets(int[] nums) {
+     
+        ans = new ArrayList<>();
+        output = new ArrayList<>();
+        int index = 0;
+        if(nums == null || nums.length == 0)
+            return ans;
+        
+        solve(nums, output, index);
+        
+        return ans;
     }
-  }
-
-  public List<List<Integer>> subsets(int[] nums) {
-    n = nums.length;
-    for (k = 0; k < n + 1; ++k) {
-      backtrack(0, new ArrayList<Integer>(), nums);
-    }
-    return output;
-  }
 }
