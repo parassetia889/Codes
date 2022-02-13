0c7c20adea0a56a9fb9e01bc1aae189e2742d4cb
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-13 11:12:31 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (25.40%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,18 +1,27 @@
 class Solution {
+  List<List<Integer>> output = new ArrayList();
+  int n, k;
+
+  public void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
+    // if the combination is done
+    if (curr.size() == k) {
+      output.add(new ArrayList(curr));
+      return;
+    }
+    for (int i = first; i < n; ++i) {
+      // add i into the current combination
+      curr.add(nums[i]);
+      // use next integers to complete the combination
+      backtrack(i + 1, curr, nums);
+      // backtrack
+      curr.remove(curr.size() - 1);
+    }
+  }
+
   public List<List<Integer>> subsets(int[] nums) {
-    List<List<Integer>> output = new ArrayList();
-    int n = nums.length;
-
-    for (int i = (int)Math.pow(2, n); i < (int)Math.pow(2, n + 1); ++i) {
-      // generate bitmask, from 0..00 to 1..11
-      String bitmask = Integer.toBinaryString(i).substring(1);
-
-      // append subset corresponding to that bitmask
-      List<Integer> curr = new ArrayList();
-      for (int j = 0; j < n; ++j) {
-        if (bitmask.charAt(j) == '1') curr.add(nums[j]);
-      }
-      output.add(curr);
+    n = nums.length;
+    for (k = 0; k < n + 1; ++k) {
+      backtrack(0, new ArrayList<Integer>(), nums);
     }
     return output;
   }
