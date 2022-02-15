cee03781f933e4d32a77e8750866332a498f8ebc
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-16 00:10:36 +0530

    Update levelOrder.java

--- BinaryTree/levelOrder.java
+++ BinaryTree/levelOrder.java
@@ -5,6 +5,7 @@ import com.sun.source.tree.Tree;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class LevelOrder {
     public static void levelOrder(TreeNode root){
@@ -35,25 +36,46 @@ public class LevelOrder {
         }
 
     }
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(3);
-        root.left.left = new TreeNode(7);
-        root.left.right = new TreeNode(11);
-        root.right = new TreeNode(5);
-        root.right.left = new TreeNode(17);
 
+    public static void buildFromLevelOrder(TreeNode root){
+        Queue<TreeNode> q = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Data for root");
+        int n = sc.nextInt();
+        root = new TreeNode(n);
+
+        q.add(root);
+        while ( !q.isEmpty()){
+            TreeNode temp = q.poll();
+
+            System.out.println("Enter left node for "+temp.val);
+            int left = sc.nextInt();
+
+            if(left != -1) {
+                temp.left = new TreeNode(left);
+                q.add(temp.left);
+            }
+            System.out.println("Enter right data for "+ temp.val);
+            int right = sc.nextInt();
+
+            if(right != -1) {
+                temp.right = new TreeNode(right);
+                q.add(temp.right);
+            }
+
+        }
         levelOrder(root);
     }
+    public static void main(String[] args) {
+//        TreeNode root = new TreeNode(1);
+//        root.left = new TreeNode(3);
+//        root.left.left = new TreeNode(7);
+//        root.left.right = new TreeNode(11);
+//        root.right = new TreeNode(5);
+//        root.right.left = new TreeNode(17);
+
+        TreeNode root = null;
+        buildFromLevelOrder(root);
+
+    }
 }
-
-
-
-/*
-
-1 
-3 5 
-7 11 17 
-
-
-*/
