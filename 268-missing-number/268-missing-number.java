e7822d710c9a68410baba817fb9827d96be01a0b
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-15 22:20:17 +0530

    Time: 1 ms (74.31%), Space: 50.7 MB (28.09%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int sum = 0;
+        for(int i : nums)
+            sum += i;
+        
+        int n = nums.length;
+        int sumOfNaturalNo = (n*(n+1))/2;
+        
+        return (sumOfNaturalNo - sum);
+    }
+}
