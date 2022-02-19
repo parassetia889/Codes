19259c3f3b13d4dd4c58038806a308c3f6674a76
shardon889 <59231061+shardon889@users.noreply.github.com>
2022-02-19 15:04:34 +0530

    Time: 307 ms (23.81%), Space: 113.6 MB (46.03%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -1,25 +1,25 @@
 class Solution {
     public int minimumDeviation(int[] nums) {
         
-        TreeSet<Integer> set = new TreeSet<Integer>();
-        
-        for(int num : nums)
-            set.add(num);
-        
-        int min = set.last() - set.first();
-        
-        while(set.first() % 2 == 1){
-            set.add(set.first()*2);
-            set.remove(set.first());
-            min = Math.min(min, set.last() - set.first());
+        TreeSet<Integer> set = new TreeSet<>();
+       
+        for(int num : nums){
+            set.add(num%2==0 ? num : num*2);
+            
         }
         
-        while(set.last()%2 == 0){
-            set.add(set.last()/2);
-            set.remove(set.last());
-            min = Math.min(min, set.last() - set.first());
-        }
-        
-        return min;
+        int min = Integer.MAX_VALUE;
+            
+            while(true){
+                int val =set.last();
+                min = Math.min(min, set.last() - set.first());
+                
+                if(val%2 != 0)
+                    break;
+                
+                set.remove(val);
+                set.add(val/2);
+            }
+     return min;     
     }
 }
