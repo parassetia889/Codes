b462fe7f07e8967c947c7a2cef7253dffe11d649
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-20 21:42:53 +0530

    Added solution - LeetHub

---
+++ 2. Leap Year - GFG/2.-leap-year.java
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+ // } Driver Code Ends
+//User function Template for Java
+
+public static void utility(int y){
+    String isLeap = "False";
+
+    //Your code below
+    //Assign True or False to isLeap
+    
+    //Your code above
+    if(y%4==0 && y%100!=0)
+        isLeap = "True";
+    else if(y%400==0)
+        isLeap = "True";
+    
+    
+    //The line below prints the output
+    System.out.println(isLeap);
+}
+
+// { Driver Code Starts.
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            int y = scn.nextInt();
+            utility(y);
+        }
+        scn.close();
+    }
+}  // } Driver Code Ends
