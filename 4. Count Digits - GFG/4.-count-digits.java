c0544ecc44546744b7e7ff24cfb31ae5b4f3545e
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-20 21:49:13 +0530

    Added solution - LeetHub

---
+++ 4. Count Digits - GFG/4.-count-digits.java
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+    
+    
+ // } Driver Code Ends
+//User function Template for Java
+
+public static void countDigits(int n){
+
+    int count = 0;
+    while(n>0){
+        n = n/10;
+        count++;
+    }
+    System.out.print(count);
+
+}
+
+// { Driver Code Starts.
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            int n = scn.nextInt();
+            countDigits(n);
+            System.out.println();
+        }
+        scn.close();
+    }
+}  // } Driver Code Ends
