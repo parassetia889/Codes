e9d0028fb601c50af2cc16732125a93d7328f02e
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-20 22:05:08 +0530

    Added solution - LeetHub

---
+++ 7. Factorial - GFG/7.-factorial.java
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+ // } Driver Code Ends
+//User function Template for Java
+
+public static int nFactorial(int n){
+    int ans = 1;
+    int i = 1;
+    while(i<=n){
+        ans *= i;
+        i++;
+    }
+    
+    return ans;
+}
+
+// { Driver Code Starts.
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            int n = scn.nextInt();
+            int ans = nFactorial(n);
+            System.out.println(ans);
+        }
+        scn.close();
+    }
+}  // } Driver Code Ends
