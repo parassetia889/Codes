81d25a6f197624e3e6d3dd7827fd82d7fcf2f74d
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-23 13:55:38 +0530

    Added solution - LeetHub

---
+++ 6. Almost Equal -Java - GFG/6.-almost-equal-java.java
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    
+	public static void main (String[] args)
+	{
+	     //taking input using Scanner class
+    	 Scanner sc = new Scanner(System.in);
+    	 
+    	 //taking total number of testcases
+    	 int t = sc.nextInt();
+    	 
+    	 boolean flag = false;
+    	 while(t-- > 0){
+    	   
+    	   //taking the 2 strings
+    	   String s1=sc.next();
+    	   String s2=sc.next();
+    	   
+    	   //creating an object of class Geeks
+    	   Geeks obj=new Geeks();
+    	   
+    	   //calling coutChars method
+    	   //of class Geeks and passing
+    	   //both the Strings and printing
+    	   //the result
+    	   System.out.println(obj.coutChars(s1,s2));
+    	   
+    	 }
+    }
+}
+
+
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+/*Function to count number of characters 
+* to make s1 and s2 equal
+* s1 : first string
+* s2 : second string
+*/
+class Geeks{
+    
+    static int coutChars(String s1, String s2)
+    {
+        
+       int a[] = new int[26];
+       for(char c : s1.toCharArray())
+            a[c-'a']++;
+    
+        for(char c : s2.toCharArray())
+            a[c-'a']--;
+            
+            int ans = 0;
+        for(int i : a)
+            ans += Math.abs(i);
+            
+            
+            return ans;
+    }
+}
+
+// { Driver Code Starts.
+  // } Driver Code Ends
