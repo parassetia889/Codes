093b3eeb1eff6ff60c958e67f96c2f0ea96e3579
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-26 15:06:38 +0530

    Added solution - LeetHub

---
+++ 8. No Parameter Lambda Expression - GFG/8.-no-parameter-lambda-expression.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+	    Scanner sc =new Scanner(System.in);
+	    
+	    //taking testcases count
+	    int t=sc.nextInt();
+	    
+	    while(t-->0)
+	    {
+	        //calling helperFunction of class Hello
+	        Hello h=helperFunction();
+	        
+	        //calling Interface method
+	        h.sayHello();
+	    }
+	}
+	
+	
+ // } Driver Code Ends
+//User function Template for Java
+
+
+/*
+This is what the interface looks like
+
+interface Hello {
+    public void sayHello();
+}
+*/
+public static Hello helperFunction() 
+{
+    //Your code here
+     return () -> {System.out.println("Hello");};
+    //Implement sayHello using lambda expression and return the object.
+    //Write this in the lambda expression: System.out.println("Hello")
+
+}
+
+// { Driver Code Starts.
+
+}
+
+interface Hello {
+    public void sayHello();
+}  // } Driver Code Ends
