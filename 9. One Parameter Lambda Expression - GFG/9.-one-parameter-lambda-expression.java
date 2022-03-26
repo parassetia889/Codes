9fa31a7da35735c0f5659ee62f27a85cd87036e6
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-26 15:07:35 +0530

    Added solution - LeetHub

---
+++ 9. One Parameter Lambda Expression - GFG/9.-one-parameter-lambda-expression.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner Class
+	    Scanner sc =new Scanner(System.in);
+	    
+	    //taking count of testcases
+	    int t=sc.nextInt();
+	    
+	    while(t-->0)
+	    {
+	        //taking n
+	        int n=sc.nextInt();
+	        
+	        //calling helperFunction of class Multiply
+	        Multiply m=helperFunction();
+	        
+	        //calling method multiplyBy5 of interace Multiply
+	        System.out.println(m.multiplyBy5(n));
+	    }
+	}
+	
+ // } Driver Code Ends
+//User function Template for Java
+
+/*
+The interface looks like
+
+interface Multiply {
+    public int multiplyBy5(int n);
+}
+*/
+
+
+public static Multiply helperFunction() 
+{
+    return (n)->n*5;
+    //Implement the multiplyBy5(int n) method using lambda expression. The implemented function should return n*5
+    
+}
+
+// { Driver Code Starts.
+
+
+}
+
+interface Multiply {
+    public int multiplyBy5(int n);
+}  // } Driver Code Ends
