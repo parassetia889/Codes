fa3ec3571f9a18997fdc7dca4aa8427a89cb81f9
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-29 23:09:11 +0530

    Added solution - LeetHub

---
+++ 9. Find Immediate Greater Than X - GFG/9.-find-immediate-greater-than-x.java
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            String str[] = read.readLine().trim().split(" ");
+            
+            int arr[] = new int[n];
+            for(int i = 0; i < n; i++)
+                arr[i] = Integer.parseInt(str[i]);
+                
+            int x = Integer.parseInt(read.readLine());    
+            System.out.println(new Solution().immediateGreater(arr, n, x));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    // Complete the function
+    public static int immediateGreater(int arr[], int n, int x)
+    {
+        int result = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i : arr){
+            if(i > x){
+                if(i < min){
+                min = i;
+                result = min;}
+            }
+        }
+        return result;
+    }
+}
