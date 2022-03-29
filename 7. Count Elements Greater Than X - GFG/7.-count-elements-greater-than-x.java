994098ea651e464735c847e1b55f54b474d827c4
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-29 23:03:47 +0530

    Added solution - LeetHub

---
+++ 7. Count Elements Greater Than X - GFG/7.-count-elements-greater-than-x.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            String str[] = read.readLine().trim().split(" ");
+            
+            int arr[] = new int[n];
+            for(int i = 0; i <n; i++)
+                arr[i] = Integer.parseInt(str[i]);
+            
+            int x = Integer.parseInt(read.readLine());
+            System.out.println(new Solution().greaterThanX(arr, n, x));
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    // arr[]: input array
+    // n: size of the array
+    // x: element for which you need to return the count
+    public static int greaterThanX(int arr[], int n, int x)
+    {
+        int count = 0;
+        for(int i : arr){
+            if(i > x)
+                count++;
+                
+        }
+        return count;
+    }
+}
