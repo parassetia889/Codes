4d748a2e4751a00b594e167136228d7acf2ee7f2
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-03-30 13:48:55 +0530

    Added solution - LeetHub

---
+++ 15. Mean And Median of Array - GFG/15.-mean-and-median-of-array.java
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Mathematics {
+    
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		//testcase
+		int T=sc.nextInt();
+		
+		//looping testcase
+		while(T-->0)
+		{
+		    Solution obj=new Solution();
+		    int N;
+		    
+		    //number of elements in array
+		    N=sc.nextInt();
+		    int a[]=new int [N];
+		    
+		    //inseting elements in the array
+		    for(int i=0;i<N;i++)
+		    {
+		        int x=sc.nextInt();
+		        a[i]=x;
+		    }
+		    
+		    //calling mean() and median() functions
+		    System.out.println(obj.mean(a,N)+" "+obj.median(a,N));
+		   
+		    
+		}
+		
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+  //Function to find median of the array elements.  
+  public int median(int A[],int N)
+    {
+      
+       Arrays.sort(A);
+       
+       //Your code here
+       //If median is fraction then conver it to integer and return
+       int k = N/2;
+       if(N%2 != 0)
+            return A[k];
+        else
+            return (A[k-1]+A[k])/2;
+    }
+    //Function to find median of the array elements.
+    public int mean(int A[],int N)
+    {
+        int sum = 0;
+       for(int i : A)
+            sum += i;
+            
+            return (int)sum/N;
+    }
+
+}
+
