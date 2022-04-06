757ec5749aebd55698a95f42771545505f2a310b
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-06 15:24:46 +0530

    Added solution - LeetHub

---
+++ 8. Find nCr - GFG/8.-find-n-cr.java
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int n  =Integer.parseInt(str[0]);
+            int r = Integer.parseInt(str[1]);
+            
+            Get obj = new Get();
+            System.out.println(obj.nCr(n, r));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Get
+{
+    public static int nCr(int n,int r)
+    {
+        if(n==r || r==0)
+            return 1;
+        
+        return (nCr(n-1, r-1) + nCr(n-1,r));
+        
+    }
+}
