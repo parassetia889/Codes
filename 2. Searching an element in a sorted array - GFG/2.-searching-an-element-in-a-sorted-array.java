32556adbb35a28b255c89b0a1928554f8b0a159d
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-10 23:05:39 +0530

    Added solution - LeetHub

---
+++ 2. Searching an element in a sorted array - GFG/2.-searching-an-element-in-a-sorted-array.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class gfg
+{
+    
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String s[] = read.readLine().trim().split("\\s+");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            
+            int arr[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = Integer.parseInt(st[i]);
+            }
+            
+            Solution obj = new Solution();
+            
+            System.out.println(obj.searchInSorted(arr, n, k));
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution{
+    static int searchInSorted(int arr[], int N, int K)
+    {
+        
+        int s = 0, e = N-1;
+        
+        while(s <= e){
+            int mid = s + (e-s)/2;
+            
+            if(arr[mid] == K)
+                return 1;
+            else if(arr[mid] < K)
+                s = mid+1;
+            else
+                e = mid-1;
+        }
+     return -1;   
+    }
+}
