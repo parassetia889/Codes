835f6fea287aa539bd11fd5e77932f52f9d24a40
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-15 09:50:59 +0530

    Time: 7 ms (12.90%), Space: 45 MB (88.31%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,31 +1,28 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
+    public int[] twoSum(int[] nums, int target) {
         
-        int n = numbers.length; 
-        for(int i = 0; i<n; i++){
+        
+        for(int i =  0; i<nums.length; i++){
             
-            int j = binarySearch(numbers, target - numbers[i], i+1);
+            int j = binarySearch(nums, target-nums[i], i+1, nums.length-1);
             if(j != -1)
-                return new int[] {i+1, j+1};
+                return new int[]{i+1, j+1};
         }
         
-        throw new IllegalArgumentException("No such sum");
+        throw new IllegalArgumentException("No such Pair");
     }
     
-    
-    public int binarySearch(int nums[], int key, int start){
-     
-        int s = start, e = nums.length - 1;
+    public int binarySearch(int[] nums, int key, int s, int e){
         
         while(s <= e){
-            
             int mid = s + (e-s)/2;
+            
             if(nums[mid] == key)
-                return mid;
+                    return mid;
             else if(nums[mid] < key)
-                s = mid + 1;
+                s = mid+1;
             else
-                e = mid - 1;
+                e = mid-1;
         }
         
         return -1;
