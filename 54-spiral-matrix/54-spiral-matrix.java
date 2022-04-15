5a37852c262930265df2442ba47696145f7e3b82
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-15 19:54:02 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (93.26%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,12 +1,12 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-      
-        List<Integer> al = new ArrayList<>();
         
+        int total = matrix.length * matrix[0].length;
+        int count = 0;
         int row = matrix.length;
         int col = matrix[0].length;
-        int total = row * col;
-        int count = 0;
+        
+        List<Integer> al = new ArrayList<Integer>();
         
         int startRow = 0, endRow = row-1;
         int startCol = 0, endCol = col-1;
@@ -14,36 +14,33 @@ class Solution {
         while(count < total){
             
             //firstRow
-            for(int i = startCol; i<=endCol && count<total;  i++){
+            for(int i = startCol; count < total && i<= endCol ; i++){
                 al.add(matrix[startRow][i]);
                 count++;
             }
             startRow++;
             
             //lastCol
-            for(int i = startRow; i<=endRow && count<total;  i++){
+             for(int i = startRow; i<= endRow && count < total; i++){
                 al.add(matrix[i][endCol]);
                 count++;
             }
             endCol--;
             
-            
             //lastRow
-            for(int i = endCol; i >= startCol && count<total;  i--){
+             for(int i = endCol; i >= startCol && count < total; i--){
                 al.add(matrix[endRow][i]);
                 count++;
             }
             endRow--;
             
-            //startCol
-            for(int i = endRow; i >= startRow && count<total;  i--){
+            //firstCol
+             for(int i = endRow; i >= startRow && count < total; i--){
                 al.add(matrix[i][startCol]);
                 count++;
             }
             startCol++;
         }
-        
-        
         return al;
     }
 }
