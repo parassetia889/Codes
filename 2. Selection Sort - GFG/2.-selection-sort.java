022a96f81bf5de01cfdd804f5e4c895b56611c26
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-16 16:47:48 +0530

    Added solution - LeetHub

--- 2. Selection Sort - GFG/2.-selection-sort.java
+++ 2. Selection Sort - GFG/2.-selection-sort.java
@@ -39,7 +39,7 @@ class Solution
 	
 	void selectionSort(int arr[], int n)
 	{
-	   for(int i = 0; i<n; i++){
+	   for(int i = 0; i<n-1; i++){
 	       int minIndex = i;
 	       for(int j = i+1; j < n; j++){
 	           if(arr[j] < arr[minIndex])
