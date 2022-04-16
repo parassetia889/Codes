6ec329ba059914d08f2747ef3fbbf2ded21ad660
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-16 09:48:00 +0530

    Time: 2 ms (82.82%), Space: 45.8 MB (90.50%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,40 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
-
-        int element = getCandidate(nums);
-        int majorityCount = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == element) {
-                majorityCount++;
-            }
-        }
-
-        if(majorityCount > nums.length / 2) {
-            return element;
-        }
-        return -1;
-
-    }
-
-
-    public int getCandidate(int [] num) {
-
-        int majorityElementIndex = 0;
-        int majorityCount = 0;
-
-        for(int i = 0; i < num.length; i++) {
-            if(num[i] == num[majorityElementIndex]) {
-                majorityCount++;
-            } else {
-                majorityCount--;
-            }
-            if(majorityCount == 0) {
-                majorityElementIndex = i;
-                majorityCount = 1;
-            }
-        }
-        return  num[majorityElementIndex];
+        
+        int count = 0;
+        Integer candidate = null;
+        
+        for(int i : nums){
+            
+            if(count == 0)
+                candidate = i;
+            
+            count += (i == candidate) ? 1 : -1;
+         }
+        
+        return candidate;
     }
 }
-
