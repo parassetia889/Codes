e88eae27473ea432ad34f413a37864535dccf0be
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-16 11:13:49 +0530

    Time: 7 ms (89.01%), Space: 45.6 MB (88.49%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        /*
+        
+        aet aet
+        */
+        
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        
+        for(String str : strs){
+            
+            
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String newStr = new String(arr);
+            
+            if(map.containsKey(newStr)){
+                
+                map.get(newStr).add(str);
+            }
+            else{
+                List<String> al = new ArrayList<>();
+                al.add(str);
+                map.put(newStr,al);
+            }
+            
+        }
+        List<List<String>> res = new ArrayList<>();
+        
+        for(Map.Entry<String, List<String>> s : map.entrySet()){
+            res.add(s.getValue());
+        }
+        
+        return res;
+    }
+}
