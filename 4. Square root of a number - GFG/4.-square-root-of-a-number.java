610dbb27bf37a67f09bbd03493a3c20768e2f477
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-16 09:28:26 +0530

    Added solution - LeetHub

--- 4. Square root of a number - GFG/4.-square-root-of-a-number.java
+++ 4. Square root of a number - GFG/4.-square-root-of-a-number.java
@@ -27,32 +27,23 @@ class Solution
 {
      long floorSqrt(long x)
 	 {
-		// Your code here
-        long start = 0;
-        long end = x/2;
-        long mid = -1;
-        
-        if(x == 1) return 1;
-        
-        while(start <= end) {
-            mid = (start + end) / 2;
-            //System.out.println(mid);
-            
-            if(mid*mid == x) {
-                return mid;
-            }
-            if(mid*mid < x && (mid+1)*(mid+1) > x) {
-                return mid;
-            }
-            
-            if(mid*mid < x) {
-                start = mid+1;
-            }
-            else if(mid*mid > x) {
-                end = mid-1;
-            }
-        }
-        
-        return -1;
+		long l = 0, h = x;
+		long ans = 0;
+		
+		while(l <= h){
+		    
+		    long  mid = l + (h-l)/2;
+		    if(mid*mid == x)
+		        return mid;
+		    
+		    else if(mid*mid < x){
+		        ans = mid;
+		        l = mid + 1;
+		    }
+		    else
+		        h = mid - 1;
+		}
+		return ans;
+		
 	 }
 }
