fa99afccbbe9df8d7770477fea22107312e96285
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-17 14:14:37 +0530

    Time: 1 ms (100.00%), Space: 42.6 MB (74.29%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -2,35 +2,32 @@ class Solution {
     public int myAtoi(String s) {
         
         String str = s.trim();
+        
         if(str.length() == 0 || str == null)
-                return 0;
-        
-        
+            return 0;
         
         boolean flag = false;
         
-        if(str.charAt(0) == '-'){ 
+        if(str.charAt(0) == '-'){
             flag = true;
             str = str.substring(1);
         }
-        else if(str.charAt(0) == '+')
+        else if(str.charAt(0)=='+'){
             str = str.substring(1);
-            
+        }
         
         double ans = 0;
         
         for(int i = 0; i < str.length(); i++){
             
             char c = str.charAt(i);
-            if(c >= '0' && c <= '9')
+            if(c >= '0' && c <='9')
                 ans = ans*10 + (c-'0');
-            
-            else
+            else 
                 break;
         }
         
         ans = flag ? -ans : ans;
-        
         if(ans > Integer.MAX_VALUE)
             return Integer.MAX_VALUE;
         
@@ -38,6 +35,6 @@ class Solution {
             return Integer.MIN_VALUE;
         
         return (int)ans;
-            
+        
     }
 }
