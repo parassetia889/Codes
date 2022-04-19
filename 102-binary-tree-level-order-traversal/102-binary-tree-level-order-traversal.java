009ce155212f9993bb1a877959cfb394e6c771ff
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-19 11:08:17 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (97.13%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -1,45 +1,35 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
         
-        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> al = new ArrayList<>();
+        Queue<TreeNode> dq = new LinkedList<>();
         
-        if(root != null)
-            q.add(root);
+        if(root == null)
+            return al;
         
-        while( !q.isEmpty())
-        {
-            int n = q.size();
-            List<Integer> ans = new ArrayList<>();
+       
+        dq.add(root);
+        
+        while( !dq.isEmpty() ){   //2
             
+            List<Integer> list = new ArrayList<>();
+            int n = dq.size();
             for(int i = 0; i<n; i++){
-                TreeNode curr = q.remove();
-                ans.add(curr.val);
                 
-                if(curr.left != null)
-                    q.add(curr.left);
+                TreeNode temp = dq.remove();
+                list.add(temp.val);
                 
-                if(curr.right!= null)
-                    q.add(curr.right);
+                if(temp.left != null)
+                    dq.add(temp.left);
+                
+                if(temp.right != null)
+                    dq.add(temp.right);
             }
-            res.add(ans);
-        }
+            al.add(list);
+        } 
         
-        return res;
+        return al;
     }
 }
