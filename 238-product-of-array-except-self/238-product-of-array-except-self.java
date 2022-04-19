1ed9134b75849e0a7acc2b8e951c0cc1e36222d9
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-19 10:37:51 +0530

    Time: 2 ms (79.21%), Space: 57.6 MB (63.21%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,36 +1,25 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         
-        int arr[] = new int[nums.length];
-       
-        int count = 0, p = 1;
+        int larr[] = new int[nums.length];
+        int rarr[] = new int[nums.length];
         
-        for(int i : nums)
-        {
-            if( i == 0)
-                count++;
-            else
-                p *= i;
+        int p = 1;
+        for(int i = 0; i<nums.length; i++){
+            larr[i] = p;
+            p *= nums[i];
         }
         
-        if(count > 1)
-            return arr;
-        else{    // count = 0,1
-            for(int i =0; i<nums.length; i++){
-                
-                if(count == 0)
-                    arr[i] = p/nums[i];
-                
-                else if(count != 0){
-                    if(nums[i] == 0)
-                        arr[i] = p;
-                    else
-                        arr[i] = 0;
-                }
-                    
-            }
+         p = 1;
+        for(int i = nums.length-1; i>=0; i--){
+            rarr[i] = p;
+            p *= nums[i];
         }
         
-        return arr;
+        
+        for(int i=0; i<nums.length; i++){
+            nums[i] = larr[i] * rarr[i];
+        }
+        return nums;
     }
 }
