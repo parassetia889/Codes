dba760c76f9bc72d90d5e0a1dbd466b5efc6a3cf
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-04-24 21:47:45 +0530

    Time: 1 ms (87.89%), Space: 42.1 MB (27.83%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        
+       List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i <= rowIndex; i++ ){
+            long ans = comb(rowIndex, i);
+            list.add((int)ans);
+        }
+        return list;
+    }
+    
+    public long comb(int n, int r){
+        long res = 1;
+        for(int i = 0; i < r; i++){
+            res *= (n - i);
+            res /= (i+1);
+        }
+        return res;
+    }
+}
