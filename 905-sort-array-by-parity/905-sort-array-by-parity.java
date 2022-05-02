c3949a661fcb27010fbdd8eab2b7f0b57b09ec7f
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-05-02 11:04:02 +0530

    Time: 2 ms (38.15%), Space: 48.1 MB (61.99%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -9,15 +9,10 @@ class Solution {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                i++; j--;
+                
             }
-            else if(nums[i]%2 == 0 && nums[j]%2 != 0){
-                i++; j--;
-            }
-            else if(nums[i]%2 == 0)
-                i++;
-            else
-                j--;
+            if(nums[i]%2 == 0) i++;
+            if(nums[j]%2 != 0) j--;
                 
             
         }
