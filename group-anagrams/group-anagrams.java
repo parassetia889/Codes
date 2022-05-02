501ab0caa62ce8ea5dbb12c9b4e6355b5403d3f2
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-05-02 23:31:02 +0530

    Time: 12 ms, Memory: 55.7 MB - LeetHub

---
+++ group-anagrams/group-anagrams.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        for(String str : strs){
+            
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String newStr = String.valueOf(ch);
+            
+            if(map.containsKey(newStr))
+                map.get(newStr).add(str);
+            else{
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(newStr,list);
+            }
+            
+        }
+        List<List<String>> res = new ArrayList<>();
+        res.addAll(map.values());
+        
+        
+        return res;
+    }
+}
