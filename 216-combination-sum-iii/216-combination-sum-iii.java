79f5de92b6e25b0a86ec0601ed8026b296d2c6e9
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-05-10 10:43:13 +0530

    Time: 1 ms (73.61%), Space: 41.4 MB (64.49%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -7,11 +7,11 @@ class Solution {
         return ans;
     }
     
-    void helper(List<Integer> li, int k, int n,  List<List<Integer>> ans, int index){
+    public void helper(List<Integer> li, int k, int n, List<List<Integer>> ans, int index){
         
-        if(li.size() == k && n == 0){
+        if(li.size() == k && n == 0 ){
             ans.add(new ArrayList<>(li));
-            return ;
+            return;
         }
         
         if(li.size() > k || n < 0)
@@ -19,8 +19,9 @@ class Solution {
         
         for(int i = index; i <= 9; i++){
             li.add(i);
-            helper(li,k, n-i, ans, i+1);
+            helper(li, k, n-i, ans, i+1);
             li.remove(li.size()-1);
         }
+        
     }
 }
