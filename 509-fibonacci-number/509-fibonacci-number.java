10f221ca6375db2e13624948673b83549be2f513
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-05-13 16:36:41 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (59.35%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -3,13 +3,15 @@ class Solution {
         
         if(n <= 1)
             return n;
-        int dp[] = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
+       int prev1 = 0, prev2 = 1;
+       int curr = 0;
+        for(int i = 2; i <= n ;i++){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+            
         
-        for(int i = 2; i <= n ;i++)
-            dp[i] = dp[i-1] + dp[i-2];
-        
-        return dp[n];
+        return curr;
     }
 }
