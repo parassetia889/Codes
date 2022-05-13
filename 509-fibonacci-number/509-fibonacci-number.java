08a42ab132a899eb3237bd4f2af09d17409ac891
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-05-13 16:33:31 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (14.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,20 +1,15 @@
 class Solution {
-   int dp[];
     public int fib(int n) {
         
-            dp = new int[n+1];
-            Arrays.fill(dp,-1);
-    return fibo(n);
-    }
-    
-    public int fibo(int n){
         if(n <= 1)
             return n;
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
         
-        if(dp[n] != -1)
-            return dp[n];
+        for(int i = 2; i <= n ;i++)
+            dp[i] = dp[i-1] + dp[i-2];
         
-        
-        return  (dp[n] = fibo(n-1) + fibo(n-2));
+        return dp[n];
     }
 }
