9952188fdf5e86c8f0d57fee90eb076e0e1e38a7
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-05-16 17:07:31 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (56.18%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,45 +1,45 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         
-        int total = matrix.length * matrix[0].length;
-        int count = 0;
-        int row = matrix.length;
-        int col = matrix[0].length;
+    
+        int row = matrix.length, col = matrix[0].length;
+        int total = row*col, count = 0;
         
-        List<Integer> al = new ArrayList<Integer>();
-        
+        List<Integer> al = new ArrayList<>();
         int startRow = 0, endRow = row-1;
         int startCol = 0, endCol = col-1;
+        
         
         while(count < total){
             
             //firstRow
-            for(int i = startCol; count < total && i<= endCol ; i++){
-                al.add(matrix[startRow][i]);
+            for(int ind = startCol;  count<total && ind<=endCol; ind++ ){
+                al.add(matrix[startRow][ind]);
                 count++;
-            }
+            } 
             startRow++;
             
-            //lastCol
-             for(int i = startRow; i<= endRow && count < total; i++){
-                al.add(matrix[i][endCol]);
+            //lastColumn
+            for(int ind = startRow; count < total && ind<=endRow; ind++){
+                al.add(matrix[ind][endCol]);
                 count++;
             }
             endCol--;
             
             //lastRow
-             for(int i = endCol; i >= startCol && count < total; i--){
-                al.add(matrix[endRow][i]);
+            for(int ind = endCol; ind >= startCol && count<total; ind--){
+                al.add(matrix[endRow][ind]);
                 count++;
             }
             endRow--;
             
-            //firstCol
-             for(int i = endRow; i >= startRow && count < total; i--){
-                al.add(matrix[i][startCol]);
+            //firstColumn
+            for(int ind = endRow; ind >= startRow && count<total; ind--){
+                al.add(matrix[ind][startCol]);
                 count++;
             }
             startCol++;
+            
         }
         return al;
     }
