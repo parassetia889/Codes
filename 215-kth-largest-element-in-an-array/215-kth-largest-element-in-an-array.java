fd0eca21e1ffa2d7571e1a43f15f3f191dea0d1a
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-05-16 22:33:42 +0530

    Time: 23 ms (10.28%), Space: 44.8 MB (43.93%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,17 +1,40 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int n = nums.length;
+        int l =0, r = n-1;
+        while(l <= r){
+            
+            int p = partition(nums,l, r);
+            if(p == (n-k))
+                return nums[p];
+            else if(p < (n-k))
+                l = p+1;
+            else
+                r  = p-1;
+        }
+        return -1;
+    }
+    
+    public int partition(int[] nums, int l, int r){
         
-        for(int i : nums)
-            pq.add(i);
+        int pivot = nums[r];
+        int i = l-1;
         
+        for(int j = l; j < r; j++){
+            
+            if(nums[j] < pivot){
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
         
-        int c = k -1;
-        while(c > 0){
-            pq.remove();
-            c--;}
+        int temp = nums[i+1];
+        nums[i+1] = pivot;
+        nums[r] = temp;
         
-        return pq.peek();
+        return i+1;
     }
 }
