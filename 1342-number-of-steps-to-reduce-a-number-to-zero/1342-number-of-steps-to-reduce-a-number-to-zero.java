eb987778962c754d29b278048b1325a9f83573ed
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-05-27 22:53:41 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (83.52%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while(num != 0){
+            count++;
+            if(num %2 != 0)
+                num -= 1;
+            else
+                num /= 2;
+        }
+        return count;
+        
+    }
+}
