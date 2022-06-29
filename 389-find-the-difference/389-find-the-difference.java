4a68d8ea5efcface56ca10fc9c3d76115fcac1e4
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-06-29 10:23:37 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (48.57%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,14 +1,14 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         
-        char ch = 0;
         
-        for(char cs : s.toCharArray())
-            ch = (char)(ch ^ cs);
+        char c = 0;
+        for(char ch : s.toCharArray())
+            c ^= ch;
+        for(char ch : t.toCharArray())
+            c ^= ch;
         
-         for(char ct : t.toCharArray())
-            ch = (char)(ch ^ ct);
         
-        return ch;
+        return c;
     }
 }
