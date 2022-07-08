7bd24f162dbf92315f6cf595dbe4710bb4c58819
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-07-08 06:21:30 +0530

    Time: 4 ms (22.43%), Space: 41.1 MB (65.80%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,23 +1,25 @@
 class Solution {
     
-    public int getNext(int n){
+    private int next(int n){
         
         int sum = 0;
-        while(n != 0){
-            sum += Math.pow(n%10, 2);
+        while(n > 0){
+            sum += Math.pow(n%10,2);
             n /= 10;
+            
         }
-        
         return sum;
     }
     
     public boolean isHappy(int n) {
         
-    Set<Integer> seen = new HashSet<>();
-        while(n  != 1 && !seen.contains(n)){
+        Set<Integer> seen = new HashSet<>();
+       
+        while(n != 1 && !seen.contains(n)){
             seen.add(n);
-            n = getNext(n);
+            n = next(n);
         }
+        
         return n == 1;
     }
 }
