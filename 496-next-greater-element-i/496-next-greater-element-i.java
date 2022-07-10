2b84ca4758207043324cb8af150690e3cc50ff51
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-07-10 23:51:08 +0530

    Time: 7 ms (35.35%), Space: 44.2 MB (50.41%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
+        int ans[] = new int[nums1.length];
         HashMap<Integer, Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
         
-        for(int i = nums2.length-1; i >= 0; i--){
-            while( !stack.empty() && stack.peek() < nums2[i]){
+        for(int i = nums2.length-1; i >=0; i--){
+            while( !stack.isEmpty() && stack.peek() < nums2[i])
                 stack.pop();
-            }
-            if(stack.empty())
-                map.put(nums2[i] , -1);
+            
+            if(stack.isEmpty())
+                map.put(nums2[i], -1);
             else
                 map.put(nums2[i], stack.peek());
             
             stack.push(nums2[i]);
         }
         
-        int[] arr = new int[nums1.length];
-        for(int i = 0; i< nums1.length; i++)
-            arr[i] = map.getOrDefault(nums1[i], -1);
+        for(int i = 0; i < nums1.length; i++){
+            ans[i] = map.getOrDefault(nums1[i],-1);
+        }
         
-        return arr;
+        return ans;
     }
 }
