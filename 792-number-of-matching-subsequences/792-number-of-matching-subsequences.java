c6f8b52810519b1f5096ab6435b1c70359bf4965
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-07-20 22:55:30 +0530

    Time: 141 ms (63.68%), Space: 54.6 MB (80.64%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -0,0 +1,37 @@
+class Solution {
+    Map<String,Boolean> map;
+    public int numMatchingSubseq(String s, String[] words) {
+        map = new HashMap<>();
+        int count = 0;
+        for(String w:words){
+            if(isSubSequence(s,w)) count++;
+        }
+        return count;
+        
+        
+    }
+    
+    private boolean  isSubSequence(String s1, String s2){
+        int p1 = 0;
+        int p2 = 0;
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if(map.containsKey(s2)) return map.get(s2);
+         
+        while(p1<len1){
+            if(s1.charAt(p1)==s2.charAt(p2)){
+                p1++;
+                p2++;
+            }else{
+                p1++;
+            }
+            if(p2==len2&&p1<=len1){
+                map.put(s2,true);
+                return true;
+            }
+        }
+        map.put(s2,false);
+        return false;
+       
+    }
+}
