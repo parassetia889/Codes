7d6b76204daf447d352a30d1e7f6f733a56667a4
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-07-25 23:54:49 +0530

    Time: 203 ms (25.00%), Space: 105.7 MB (25.00%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.java
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.java
@@ -2,46 +2,49 @@ class Solution {
     public long countExcellentPairs(int[] nums, int k) {
         
         
-        int n = nums.length;
-        HashSet<Integer> set = new HashSet();
-        
+        HashSet<Integer> set = new HashSet<>();
+       
         for(int i : nums)
             set.add(i);
         
-        n = set.size();
-        int[] dist = new int[n];
+        int n = set.size();
+        int[] dis = new int[n];
+        
         
         int i = 0;
         for(int num : set)
-            dist[i++] = Integer.bitCount(num);
+            dis[i++] = Integer.bitCount(num);
         
-        Arrays.sort(dist);
+        
+        Arrays.sort(dis);
         long ans = 0;
         
-        for( i = 0 ; i < dist.length; i++){
+        for(i = 0; i < n; i++){
             
-            int ind = bSearch(dist, k-dist[i]);
-            if(ind >= 0)
-                  if (ind >= 0) ans += (n - ind + 0l);
+            int ind = bSearch(dis, k-dis[i]);
+            if(ind != -1)
+                ans += (n-ind + 0l);
         }
+        
         return ans;
-            
     }
-    
     
     private int bSearch(int arr[], int t){
         
         int s = 0, e = arr.length-1;
         int ans = -1;
+        
         while(s <= e){
-            int mid = (s+e)/2;
+            
+            int mid = s + (e-s)/2;
             
             if(arr[mid] < t)
-                s = mid+1;
+                s = mid + 1;
             else{
                 ans = mid;
                 e = mid-1;
-            }       
+            }
+            
         }
         return ans;
     }
