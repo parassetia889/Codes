a2840ebfb8405d83636bbdda6c90c2ba534fed44
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-07-26 22:29:31 +0530

    Time: 1 ms (76.92%), Space: 40.2 MB (84.62%) - LeetHub

---
+++ 2347-best-poker-hand/2347-best-poker-hand.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String bestHand(int[] ranks, char[] suits) {
+        
+        Set<Character> set = new HashSet<>();
+        for(char ch : suits)
+            set.add(ch);
+        
+        if(set.size() == 1)
+            return "Flush";
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : ranks){
+            map.put(i, map.getOrDefault(i,0)+1);
+            
+            if(map.get(i) == 3)
+                return "Three of a Kind";}
+        
+        if(map.size() == 5)
+            return "High Card";
+        else 
+            return "Pair";
+    }
+}
