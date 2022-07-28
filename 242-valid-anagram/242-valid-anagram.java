e10c226b2804511f1e316d0d5e1410bf54db1c16
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-07-28 11:28:51 +0530

    Time: 4 ms (85.70%), Space: 44.4 MB (44.12%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        
         if(s.length() != t.length())
             return false;
         
-        int arr[] = new int[26];
-        
+        int[] arr = new int[26];
         for(char ch : s.toCharArray())
-            arr[ch - 'a']++;
+            arr[ch-'a']++;
         
-        for(char ch : t.toCharArray()){
-            if(--arr[ch - 'a'] < 0)
+        for(char ch : t.toCharArray())
+            if(--arr[ch-'a'] < 0)
                 return false;
-        }
+        
+        
         return true;
     }
 }
