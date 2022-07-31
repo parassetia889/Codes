1a894fe8c00e9561cddf37403169eb318d62209c
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-07-31 13:17:53 +0530

    Time: 160 ms (41.35%), Space: 129 MB (50.15%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -0,0 +1,38 @@
+class NumArray {
+
+    private List<Integer>  list = new ArrayList<>();
+    int total = 0;
+    
+    public NumArray(int[] nums) {
+       for(int i : nums){
+           list.add(i);
+           total += i;
+       }
+     
+    }
+    
+    public void update(int index, int val) {
+        total = total - list.get(index) + val;
+        list.set(index, val);
+    }
+    
+    public int sumRange(int left, int right) {
+    
+        int sum = total;
+        for(int i = 0; i < left; i++)
+            sum -= list.get(i);
+        
+        for(int i = right+1 ; i < list.size(); i++)
+            sum -= list.get(i);
+        
+        
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
