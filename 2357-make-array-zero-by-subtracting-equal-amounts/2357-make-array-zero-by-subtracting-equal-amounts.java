892ec9d6cf692c67ebecce5e290c58250364889c
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-07-31 21:55:46 +0530

    Time: 7 ms (25.00%), Space: 42.2 MB (25.00%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,8 +1,5 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        
-//      return (int)Arrays.stream(nums).filter(a -> a > 0).distinct().count();
-
-        return (int)Arrays.stream(nums).filter( a -> a > 0).distinct().count();
+            return (int)Arrays.stream(nums).filter(a -> a > 0).distinct().count();
     }
 }
