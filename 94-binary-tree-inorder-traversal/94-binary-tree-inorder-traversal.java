826edc1e1d7b03e993b2ed8b9ee163a0de801d2a
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-08-04 17:47:22 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (95.07%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,21 +15,25 @@
  */
 class Solution {
     
-       
-        public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> list;
+    public List<Integer> inorderTraversal(TreeNode root) {
         
-             List<Integer> res= new ArrayList<>();
-                helper(root, res);
-            
-            return res;
-        }
-    
-    public void helper(TreeNode root, List<Integer> res ){
-        if(root != null){
-            helper(root.left, res);
-           res.add(root.val);
-            helper(root.right, res);
-        }
+        list = new ArrayList<>();
+        inorder(root);
+        return list;
     }
-
+    
+    
+    private void inorder(TreeNode root){
+        if(root == null)
+            return ;
+        
+        if(root.left != null)
+            inorder(root.left);
+        
+        list.add(root.val);
+        
+        if(root.right != null)
+            inorder(root.right);
+    }
 }
