fa2e91b58152aa7d13dc4e3133f58fd98e8626b0
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-08-27 19:33:11 +0530

    Time: 1 ms (99.13%), Space: 44.4 MB (52.93%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -14,25 +14,22 @@
  * }
  */
 class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+    
     public int height(TreeNode root){
         if(root == null)
             return 0;
         
         int left = height(root.left);
-        if(left == -1)
-            return -1;
+        if(left == -1)  return -1;
         
         int right = height(root.right);
-        if(right == -1)
-            return -1;
+        if(right == -1)    return -1;
         
-        if(Math.abs(left - right) > 1)
-            return -1;
+        if(Math.abs(left-right) > 1)    return -1;
         
-        return Math.max(left, right)+1;
-    }
-    
-    public boolean isBalanced(TreeNode root) {
-        return height(root) != -1;
+        return 1+Math.max(left,right);
     }
 }
