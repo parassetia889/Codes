e9a9c5c1fe1502a711f4d8b6d3667ca790a34f37
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-08-29 11:31:47 +0530

    Time: 5 ms (55.04%), Space: 57.6 MB (38.69%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,27 +1,32 @@
 class Solution {
+    
     public int numIslands(char[][] grid) {
-        
+      
         int count = 0;
-        for(int i = 0; i<grid.length; i++){
-            for(int j = 0; j<grid[0].length; j++){
-                    if(grid[i][j] == '1'){
-                        dfs(grid, i , j);
-                        count++;
-                    }
+        for(int i = 0 ; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    count++;
+                }
             }
+           
         }
-        return count;
+         return count;
     }
     
-    public void dfs(char[][] grid, int i, int j){
+    
+    public void dfs(char[][] grid, int x, int y){
         
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length|| grid[i][j] =='0')
-            return;
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == '0')
+            return ;
         
-        grid[i][j] = '0';
-        dfs(grid,i,j+1);
-        dfs(grid,i+1, j);
-        dfs(grid,i,j-1);
-        dfs(grid,i-1, j);
+        grid[x][y] = '0';
+        
+        dfs(grid,x,y+1);
+        dfs(grid, x+1,y);
+        dfs(grid,x,y-1);
+        dfs(grid,x-1,y);
+       
     }
 }
