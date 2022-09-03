adeb3201556d5d888cc3586e18569323d2d3ea75
Paras Setia <59231061+shardon889@users.noreply.github.com>
2022-09-03 17:45:08 +0530

    Time: 6 ms (47.50%), Space: 41.9 MB (97.81%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,32 +1,26 @@
 class Solution {
-
-    public int[] numsSameConsecDiff(int N, int K) {
-
-        if (N == 1)
-            return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        List<Integer> queue = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        for(int level = 1; level < N; ++ level) {
-            ArrayList<Integer> nextQueue = new ArrayList<>();
-            // iterate through each number within the level
-            for (Integer num : queue) {
-                Integer tailDigit = num % 10;
-
-                ArrayList<Integer> nextDigits = new ArrayList<>();
-                nextDigits.add(tailDigit + K);
-                if (K != 0)
-                    nextDigits.add(tailDigit - K);
-                for (Integer nextDigit : nextDigits) {
-                    if (0 <= nextDigit && nextDigit < 10) {
-                        Integer newNum = num * 10 + nextDigit;
-                        nextQueue.add(newNum);
-                    }
-                }
-            }
-            // prepare for the next level
-            queue = nextQueue;
+    public int[] numsSameConsecDiff(int n, int k) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int num = 1; num < 10; num++)
+            dfs(num, num,  k,n-1 , list);
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+    
+    private void dfs(int num, int digit, int k, int n, List<Integer> list){
+        
+        if(n == 0){
+            list.add(num);
+            return;
         }
-
-        return queue.stream().mapToInt(i->i).toArray();
+        
+        if(digit+k <= 9)
+            dfs(num*10+(digit+k), digit+k, k, n-1, list);
+        
+        
+        if( k!= 0 && digit-k >= 0)
+            dfs( num*10+(digit-k), digit-k, k,n-1, list);
     }
 }
